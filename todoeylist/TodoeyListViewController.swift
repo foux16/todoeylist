d574ece333a2b5aeb2ed66f0c7f90c4368400da5
Felipe Silva Lima <foux16@gmail.com>
2019-01-28 15:22:06 -0300

    to do List complete with datasource and delegates methods

---
+++ todoeylist/TodoeyListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  todoeylist
+//
+//  Created by Felipe Silva Lima on 1/28/19.
+//  Copyright © 2019 Felipe Silva Lima. All rights reserved.
+//
+
+import UIKit
+
+class TodoeyListViewController: UITableViewController {
+
+    //Criacao de variaveis
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demorgogon"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - Table View Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    //MARK - Table View Delegate Methods
+    //Metodo que verifica qual celula foi selecionada.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //Utilizando uma caracteristica da table view chamada acessory para marcar/desmarcar as tarefas selecionadas.
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
